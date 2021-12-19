3627f166877797a068777f363d9df530dbf33c3d
hassan-shafique <muhammadhassanshafique5@GMAIL.COM>
2021-12-19 22:50:15 +0500

    Live Chat

--- assets/js/script_home.js
+++ assets/js/script_home.js
@@ -22,6 +22,16 @@ $(document).ready(function(){
 	$(".wish-icon i").click(function(){
 		$(this).toggleClass("fa-heart fa-heart-o");
 	});
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+      s1.async=true;
+      s1.src='https://embed.tawk.to/61bf700f80b2296cfdd27015/1fn9srfmf';
+      s1.charset='UTF-8';
+      s1.setAttribute('crossorigin','*');
+      s0.parentNode.insertBefore(s1,s0);
+    })();
+
 });	
 function opensearch()
 {
@@ -32,7 +42,8 @@ function closesearch()
   $(".search-header").removeClass("search-open");
 }
 function dismiss_Preloader() {
-  setTimeout(function () {
+  
     $(".preloader").css('display','none');
-  }, 3000);
+  
 }
+
