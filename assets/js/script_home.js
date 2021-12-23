91feed041a44aaf27d30e46df83d4009d6482595
hassan-shafique <muhammadhassanshafique5@GMAIL.COM>
2021-12-23 20:49:53 +0500

    Offcanvas

--- assets/js/script_home.js
+++ assets/js/script_home.js
@@ -11,7 +11,7 @@ $(window).scroll(function(e){
     var $el = $('.fixedElement'); 
     var isPositionFixed = ($el.css('position') == 'fixed');
     if ($(this).scrollTop() > 90 && !isPositionFixed){ 
-      $el.css({'position': 'fixed', 'top': '0px', 'right': '0px', 'left': '0px', 'z-index': '200'}); 
+      $el.css({'position': 'fixed', 'top': '0px', 'right': '0px', 'left': '0px', 'z-index': '4'}); 
     }
     if ($(this).scrollTop() < 90 && isPositionFixed){
       $el.css({'position': 'static', 'top': '0px'}); 
@@ -41,11 +41,73 @@ function closesearch()
 {
   $(".search-header").removeClass("search-open");
 }
+function closeOffcanvas() {
+  document.getElementsByTagName('body')[0].classList.toggle('nav-open');
+}
 function dismiss_Preloader() {
   setTimeout(() => {  
     $(".preloader").css('display','none');
    }, 1000);
-    
-  
 }
 
+$(document).ready(function() {
+  var navTrigger = document.getElementsByClassName('nav-trigger')[0],
+    body = document.getElementsByTagName('body')[0];
+
+navTrigger.addEventListener('click', toggleNavigation);
+
+function toggleNavigation(event) {
+  event.preventDefault();
+  body.classList.toggle('nav-open');
+}
+});
+
+(function ($) {
+  "use strict";
+  /*==================================================================
+  [ Validate ]*/
+  var input = $('.validate-input .input100');
+
+  $('.validate-form').on('submit',function(){
+      var check = true;
+
+      for(var i=0; i<input.length; i++) {
+          if(validate(input[i]) == false){
+              showValidate(input[i]);
+              check=false;
+          }
+      }
+      return check;
+  });
+
+  $('.validate-form .input100').each(function(){
+      $(this).focus(function(){
+         hideValidate(this);
+      });
+  });
+
+  function validate (input) {
+      if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+          if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+              return false;
+          }
+      }
+      else {
+          if($(input).val().trim() == ''){
+              return false;
+          }
+      }
+  }
+
+  function showValidate(input) {
+      var thisAlert = $(input).parent();
+
+      $(thisAlert).addClass('alert-validate');
+  }
+
+  function hideValidate(input) {
+      var thisAlert = $(input).parent();
+
+      $(thisAlert).removeClass('alert-validate');
+  }
+})(jQuery);
